ad9d394af32fe02b6ac2e93dc9bf3916f522e1d5
unknown <neethupmohan26@gmail.com>
2022-03-24 11:14:17 +0530

    second commit

--- todoprojectapp/models.py
+++ todoprojectapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime as dtm
 
 # Create your models here.
+class Register(models.Model):
+    
+# user details
+    Users = models.OneToOneField(User,on_delete=models.CASCADE)
+    Phone_number = models.CharField(max_length=10)
+    Address = models.CharField(max_length=100)
+    DOB = models.DateField(auto_created=False,auto_now=False)
+    DOJ = models.DateTimeField(auto_created=False,auto_now=False,default=dtm.now)
+    Image = models.ImageField('media')
